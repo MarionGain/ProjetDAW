8fcdb7daeedb348956b29242e872cfb0639935ec
Gary-King <60600955+Gary-King@users.noreply.github.com>
2020-03-08 18:21:04 +0100

    Add files via upload

---
+++ Mathieu - Inscription Connexion avec BDD et Session/projet/VUE/connexion_reussiteVIEW.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$_SESSION['Login'] = $_POST['Login'];
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="../styles/inscription.css">
+    <link type="text/css" rel="stylesheet" href="../styles/header_footer.css">
+    <title>Connexion Reussite</title>
+  </head>
+  <body>
+    <?php
+      require('../header_footer/header.php');
+      ?>
+
+<h1  >Bravo ! vous êtes connecté !</h1>
+<p style="text-align:center">votre login de session est : <?php echo $_SESSION['Login']; ?></p>
+<h1>
+  <a href="../page.php">GO !!!!</a>
+</h1>
+    <?php  require('../header_footer/footer.php');
+    ?>
+  </body>
+</html>
