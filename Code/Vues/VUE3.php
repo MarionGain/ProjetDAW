7172b7838d977e4643ab8411ac362c06d38a01fb
BaptisteChe <BaptisteChe@users.noreply.github.com>
2020-03-08 18:58:11 +0100

    Tableau php

--- Code/Vues/VUE3.php
+++ Code/Vues/VUE3.php
@@ -2,6 +2,7 @@
 <html lang="fr">
   <head>
     <link href="style.css" rel="stylesheet">
+	<link href="tableau.css" rel="stylesheet">
     <meta charset="utf-8">
     <title></title>
   </head>
@@ -9,9 +10,15 @@
     <?php require("header.php"); ?>
   </header>
   <body>
+  <div id=carte>
   <?php require("menu.php"); $menu=affiche_menu(); echo $menu;
   require("map.php");
-    echo $map; ?>
+  echo $map;
+  ?>
+  </div>
+  <nav id=tab>
+	<?php require("tableau.php"); ?>
+  </nav>
   </body>
   <footer>
     <?php require("footer.php"); ?>
