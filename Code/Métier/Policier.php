cf687c36e8747de19de455de15765f3bc65a65a4
HakyoSama <60430479+HakyoSama@users.noreply.github.com>
2020-03-14 14:06:14 +0100

    Proposition classe Personnage

--- Code/Métier/Policier.php
+++ Code/Métier/Policier.php
@@ -1,7 +1,22 @@
-<?php 
-class Policier extends Role  
+<?php
+class Policier extends Personnage
 {
-    
+  function __construct() {
+        parent::__construct(); //appel du constructeur de Personnage car Policier descends de Personnage
+        print "Constructeur de Policier\n";
+    }
+
+  function __destruct() {
+        print "Destruction de " . __CLASS__ . "\n";
+    }
+
+  public function __set($property, $value) {
+        parent::__set($property,$value); //appel du set de Personnage car Policier descends de Personnage
+    }
+
+  public function __get($property) {
+        parent::__get($property); //appel du get de Personnage car Policier descends de Personnage
+    }
 
 }
 ?>
--- Code/Métier/Pompier.php
+++ Code/Métier/Pompier.php
@@ -1,7 +1,21 @@
-<?php 
-class Pompier extends Role 
+<?php
+class Pompier extends Personnage
 {
-  
+  function __construct() {
+        parent::__construct(); //appel du constructeur de Personnage car Pompier descends de Personnage
+        print "Constructeur de pompier\n";
+    }
 
+  function __destruct() {
+        print "Destruction de " . __CLASS__ . "\n";
+    }
+
+  public function __set($property, $value) {
+        parent::__set($property,$value); //appel du set de Personnage car Pompier descends de Personnage
+    }
+
+  public function __get($property) {
+        parent::__get($property); //appel du get de Personnage car Pompier descends de Personnage
+    }
 }
 ?>
