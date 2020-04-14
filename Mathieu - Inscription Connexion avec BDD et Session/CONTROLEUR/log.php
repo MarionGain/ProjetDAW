25a53069fe68174f233add8e2c88c00962fad5c6
Gary-King <60600955+Gary-King@users.noreply.github.com>
2020-04-14 12:32:45 +0200

    Add files via upload

---
+++ Mathieu - Inscription Connexion avec BDD et Session/CONTROLEUR/log.php
@@ -0,0 +1,45 @@
+<?php
+$erreur=0;
+//********************* Traitement de la page demandée, via la variable ACTION pour s'inscrire ou se connecter
+  if( isset( $_GET['action'] ) )
+    {
+      switch ( $_GET['action'] )
+        {
+          case 'inscription':
+                          require('../VUE/inscription.php');
+                          break;
+          case 'connexion'  :
+                          require('../VUE/connexion.php');
+                          break;
+          default           :
+                          $erreur=1;
+                          break;
+        }
+    }
+  else
+    {
+      $erreur=2;
+    }
+
+//********************* Afficher la nature de l'erreur lors du chargement de la page , si $erreur == 0 --> pas d'erreur
+if($erreur!=0)
+  {
+    echo ' <link type="text/css" rel="stylesheet" href="../styles/header_footer.css">';
+    echo ' <link type="text/css" rel="stylesheet" href="../styles/erreur.css">';
+    require('../header_footer/header.php');
+
+    if($erreur==1)
+      {
+        echo "<h1 class='erreur_404'> Erreur !  <br/>  La page <span style='color:red'>".$_GET['action']."</span> n'existe pas  </h1>";
+        echo "<p id='bouton_retour_acceuil'><a style='text-decoration: none' href='../log_in_ou_up.php'>Retourner à l'acceuil &gt&gt</a></p>";
+      }
+
+    else
+      {
+        echo "<h1 class='erreur_404'> Erreur ! <br/> La variable ' <span style='color:red'> action </span> ' n'est pas définie ! </h1>";
+        echo "<p id='bouton_retour_acceuil'><a style='text-decoration: none' href='../log_in_ou_up.php'>Retourner à l'acceuil &gt&gt</a></p>";
+      }
+
+    require('../header_footer/footer.php');
+  }
+ ?>
---
+++ Mathieu - Inscription Connexion avec BDD et Session/VUE/AffichageRequete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<p>resultat connexion : </p>
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
