28d217d8e85fcc5d2cc5de132f2c7935015ce292
MarionGain <marion.gain@outlook.fr>
2020-04-24 19:51:25 +0200

    Ajout des fichiers nécessaires pour la vue PMA
    Ajout d'une vue DSM avec une popup PMA qui apparaît lors du clic sur l'icône du PMA.

--- Code/Vues/map.php
+++ Code/Vues/map.php
@@ -4,11 +4,12 @@
 	<title></title>
 	<meta charset="utf-8">
 	<link href="map.css" rel="stylesheet">
-	<script type="text/script" src="draganddrop.js" ></script>
+	<!-- <script type="text/script" src="draganddrop.js" ></script> -->
+	<script type="text/javascript" src="demandeRenfort.js" ></script>
 </head>
-<body>
-
-	<button>Demander des renforts</button>
+<body onload="initialisation()">
+	
+	<button id="demandeRenfort">Demander des renforts</button>
 	<?php
 	 $map = "<iframe  id=\"dropper\" frameborder=\"0\" src=\"https://www.google.com/maps/embed/v1/view?zoom=12&center=47.3220%2C5.0415&key=AIzaSyBVB41SiCcJgos7O9RfCP4cR6p6L7Xu3dc\" allowfullscreen></iframe>";
 	?>
