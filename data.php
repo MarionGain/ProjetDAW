e4cf912020766220e58ad5860500ee881d0b6c99
Antoine Baudoin <56269056+antoinebaudoin@users.noreply.github.com>
2020-04-27 16:41:24 +0200

    Les cookies pour data.php

--- data.php
+++ data.php
@@ -1,9 +1,18 @@
 <?php
+
 	$nbPompiers = 5;
 	$nbCamionPompier = 2;
 	$nbAmbulance = 3;
 	$nbMedecin = 4;
 	$nbInfirmiere = 8;
-	$nbPolier = 3;
+	$nbPolicier = 3;
 	$nbVoiturePolice = 2;
+
+	setcookie("pompier",$nbPompiers);
+	setcookie("camionPompier",$nbCamionPompier);
+	setcookie("ambulance",$nbAmbulance);
+	setcookie("medecin",$nbMedecin);
+	setcookie("infirmiere",$nbInfirmiere);
+	setcookie("policier",$nbPolicier);
+	setcookie("voiturePolice",$nbVoiturePolice);
 ?>
